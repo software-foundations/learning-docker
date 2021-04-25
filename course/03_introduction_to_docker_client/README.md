efdfc178191f7bcd2fc1a3544e3de05a641654f5
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-04-25 15:00:44 -0300

    [course][coder](README.md, and run_docker_client_01.sh) -> 20: run always create new containers

--- course/03_introduction_to_docker_client/README.md
+++ course/03_introduction_to_docker_client/README.md
@@ -11,6 +11,8 @@ sudo docker container run hello-world
 ```
 
 # Run command
+---
+Always create new containers
 
 - Concatenate 4 commands:
 1. Docker image pull (Download image from Registry)
@@ -39,4 +41,38 @@ sudo docker container ps -a
 ```console
 sudo docker container run --rm debian
 sudo docker container run --rm debian bash --version
+```
+
+- Run a container in (i)terative mode in (t)erminal, entering in the container terminal
+```console
+sudo docker container run -it debian bash
+```
+
+Doing things in container terminal
+```console
+touch docker-course.txt
+ls docker-course.txt
+ls
+```
+
+Going out the container
+```console
+exit
+```
+
+Create the container again and check the docker-course.txt file
+is not there anymore, because it is a new container
+```console
+sudo docker container run -it debian bash
+ls docker-course.txt
+
+	ls: cannot access 'docker-course.txt': No such file or directory
+
+
+exit
+```
+
+Observe Container Names given by docker
+```console
+sudo docker container ps -a
 ```
--- course/03_introduction_to_docker_client/run_docker_client_01.sh
+++ course/03_introduction_to_docker_client/run_docker_client_01.sh
@@ -6,6 +6,8 @@ docker container run hello-world
 
 #############
 # Run command
+# Always create new containbers
+
 # - Concatenate 4 commands:
 # 1. Docker image pull (Download image from Registry)
 # 2. Docker container create (Create the container)
@@ -38,3 +40,36 @@ sudo docker container ps -a
 # -> run a container and remove it after its execution
 sudo docker container run --rm debian
 sudo docker container run --rm debian bash --version
+
+# -> Different modes on docker
+
+# 1. Daemon: Execute the container in background
+# 2. Interative: To test, set and explore containers
+
+#####################
+# -> Interactive mode
+#####################
+
+# -> Run a container in (i)terative mode in (t)erminal, entering in the container terminal
+sudo docker container run -it debian bash
+
+# Doing things in container terminal
+touch docker-course.txt
+ls docker-course.txt
+ls
+
+# Going out the container
+exit
+
+# Create the container again and check the docker-course.txt file
+# is not there anymore, because it is a new container
+sudo docker container run -it debian bash
+ls docker-course.txt
+
+	# ls: cannot access 'docker-course.txt': No such file or directory
+
+
+exit
+
+# Observe Container Names given by docker
+sudo docker container ps -a
