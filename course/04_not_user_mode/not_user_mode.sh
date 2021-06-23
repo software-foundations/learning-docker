fc9b76366ccc0d881d05f1ec3ecaff40e37bef6e
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-06-23 11:18:57 -0300

    [course][coder](course/04_not_user_mode/not_user_mode.sh) -> 37: Differences between container and image

---
+++ course/04_not_user_mode/not_user_mode.sh
@@ -0,0 +1,7 @@
+#########################################
+# Differences between container and image
+#########################################
+
+sudo docker container --help
+sudo docker image --help
+sudo docker volumne --help
